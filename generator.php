77e984bbcfcaab5db354f3457e67f803e279033a
Jakub Radzik <42437524+radzikoska123@users.noreply.github.com>
2020-09-04 19:31:43 +0200

    receive data by php

--- generator.php
+++ generator.php
@@ -8,11 +8,20 @@
 <body>
     <?php
     
-        echo var_dump($_POST);
     
-    
-    
-    
+        $db_name = $_POST['db-name'];
+        $tab_name = $_POST['tab-name'];
+        $arr_col_names = [];
+        $arr_data_types = [];
+
+
+        for ($i=1; $i <= (count($_POST)-2)/2 ; $i++) { 
+            $arr_col_names[$i-1]=$_POST['col-name-'.strval($i)];
+            $arr_data_types[$i-1]=$_POST['data-type-'.strval($i)];
+        }
+
+        echo var_dump($arr_col_names);
+        echo var_dump($arr_data_types);
     
     
     
