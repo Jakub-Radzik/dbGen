46b532bbee094aca91522f79902092a85ed7b398
Jakub Radzik <42437524+radzikoska123@users.noreply.github.com>
2020-09-04 11:07:23 +0200

    Create auto increment table

---
+++ js/input_control.js
@@ -0,0 +1,50 @@
+
+var line = `
+<td class="lp">
+
+</td>
+
+<td class="col-name">
+<input type="text">
+</td>
+
+<td class="type">
+<select name="" id="">
+    <option value="Select type">Select type</option>
+</select>
+</td>
+
+<td class="example">
+
+</td>`;
+
+function add_line(){
+
+    //FIND LAST LP 
+    let numbers = document.querySelectorAll('.lp');
+    let ar = [];
+    for (let i = 0; i < numbers.length; i++) {
+        ar.push(parseInt(numbers[i].innerHTML.trim()));
+    }
+    let new_lp = Math.max.apply(null, ar);
+    new_lp++;
+    
+    let place = document.querySelector('#liner');
+    let new_child = `
+    <td class="lp">
+    ${new_lp}
+    </td>
+    <td class="col-name">
+    <input type="text">
+    </td>
+    <td class="type">
+    <select name="" id="">
+        <option value="Select type">Select type</option>
+    </select>
+    </td>
+    <td class="example">
+    </td>`;
+    
+    let place_code =place.innerHTML + new_child;
+    place.innerHTML=place_code;
+}
